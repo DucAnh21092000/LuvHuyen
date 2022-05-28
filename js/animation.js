3c5f6d20f3aff110e669b906ea553499377eccec
DucAnh21092000 <dinhanh21092000@gmail.com>
2022-05-29 00:19:10 +0700

    done

---
+++ js/animation.js
@@ -0,0 +1,115 @@
+//          Tác giả : Đinh Đức Anh
+//          Mail    : anh.dd182330@sis.hust.edu.vn
+//                    dinhanh21092000@gmail.com
+
+// Object chứa giá trị cần hiển thị
+let initialValue = {
+    content1: ` Người anh thương chỉ có một. người anh nhớ chỉ có một.
+                        Người anh yêu chỉ có một.
+                        Người anh muốn sống trọn đời cũng chỉ có một.
+                        Đó chính là em – người yêu xinh ngoan của anh.`,
+    content2: `
+      Anh chỉ có một tình yêu duy nhất.
+                        Dành tặng em bây giờ và mãi mãi.
+                        Dẫu anh đi đến tận cùng trái đất.
+                        Vẫn hướng về em bằng trọn con tim.`
+}
+
+const year = document.querySelector('.mess')
+let arrContent1 = initialValue.content1.split('')
+let arrYearResult1 = []
+
+let arrContent2 = initialValue.content2.split('')
+let arrYearResult2 = []
+
+// function chạy đầu tiên
+function display() {
+    setTimeout(() => {
+        const timeOut = setInterval(() => {
+            arrYearResult1.push(arrContent1.shift())
+            year.classList.add('.animation')
+            year.innerHTML = arrYearResult1.join("")
+            year.classList.remove('.animation')
+            if (arrContent1.length == 0) {
+                clearInterval(timeOut)
+                arrYearResult2.push(arrContent2.shift())
+                year.classList.add('.animation')
+                year.innerHTML = arrYearResult2.join("")
+
+                setTimeout(() => {
+                    display2()
+                }, 500)
+            }
+        }, 100)
+    }, 600)
+
+}
+
+function display2() {
+    setTimeout(() => {
+        const timeOut = setInterval(() => {
+            arrYearResult2.push(arrContent2.shift())
+            year.classList.add('.animation')
+            year.innerHTML = arrYearResult2.join("")
+            year.classList.remove('.animation')
+            if (arrContent2.length == 0) {
+                clearInterval(timeOut)
+                let btn = document.querySelector('.btnCtn')
+                btn.classList.remove('d-none')
+            }
+        }, 100)
+    }, 600)
+
+}
+display()
+
+const showabcd = () => {
+    let abcd = document.querySelector('.abcd')
+    abcd.classList.remove('d-none')
+}
+
+const start = () => {
+    let header = document.querySelector('.header')
+    let start = document.querySelector('.show__start')
+    start.classList.add('d-none')
+    header.classList.remove('d-none')
+}
+
+const showbtn = () => {
+    let btn = document.querySelector('.no')
+    btn.classList.remove('d-none')
+}
+
+const hidebtn = () => {
+    let btn = document.querySelector('.no')
+    btn.classList.add('d-none')
+}
+
+const wait = () => {
+    let header = document.querySelector('.header')
+    let huyen = document.querySelector('.huyen')
+    huyen.classList.remove('d-none')
+    header.classList.add('d-none')
+}
+
+const send = () => {
+    let mess = document.querySelector('.mess1')
+    let data = {
+        content: mess.value
+    }
+    fetch('https://ducanh-store.herokuapp.com/api/videos', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(() => {
+            let show = document.querySelector('.show')
+            show.classList.add('d-none')
+            let show2 = document.querySelector('.show2')
+            show2.classList.remove('d-none')
+
+        })
+
+}
